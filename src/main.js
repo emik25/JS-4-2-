ad7a9b9b9002230d104a47b6fa3838191135a38b
Emil <115448156+emik25@users.noreply.github.com>
2022-10-28 10:34:12 +0500

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,15 @@
+// 2
+let name = prompt('What is your name?').toLowerCase().trim()
+let age = prompt('How old are u?')
+let userMoney = prompt('How much money?')
+let money = 100
+
+
+
+if(name['a'] === 'a'){
+    console.log('Hello ' + name);
+} else if(age >=20 && age <=40 && userMoney >= money){
+    console.log('Welcome');
+} else{
+    console.log('Go home');
+}
